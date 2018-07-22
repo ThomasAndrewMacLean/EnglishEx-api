e49c1b618555b6fbd83c54e06af43f1ae8265f92
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-07-22 19:40:26 +0200

    added ex routes

--- app.js
+++ app.js
@@ -28,6 +28,61 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+function getUserEmailFromToken(req, res, next) {
+
+    console.log(process.env.NODE_ENV);
+
+
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        console.log(bearerToken);
+        try {
+            let authData = jwt.verify(bearerToken, process.env.JWT_SECRET); //, (err, authData) => {
+            console.log(authData);
+
+            const email = authData.user.email;
+            console.log('EEEEemail');
+            console.log(email);
+
+            if (email === 'thomas.maclean@gmail.com') {
+                req.admin = true;
+            }
+            users.findOne({
+                email
+            }).then(user => {
+                if (user.confirmed) {
+                    console.log('USER IS CONFIRMED');
+
+                    req.token = email;
+                    next();
+                } else {
+                    console.log('CONFIRMMMMM???');
+
+                    res.status(403).json({
+                        message: 'not yet confirmed!'
+                    });
+                }
+            }).catch(err => {
+                console.log('TIS NENERREUR');
+
+                console.log(err);
+                res.status(403).json(err);
+            });
+        } catch (error) {
+            console.log('eRROOOOOORRRRR');
+            console.log(error);
+            res.status(403).json(error);
+        }
+        
+    } else {
+        res.status(403).json({
+            err: 'no authorization token!'
+        });
+    }
+}
+
 app.get('/ping', (req, res) => {
     res.status(200).json('pong');
 });
@@ -132,6 +187,78 @@ app.post('/signup', (req, res) => {
             err
         });
     });
+});
+
+app.post('/login', (req, res) => {
+    const {
+        password,
+        email
+    } = req.body;
+    users.findOne({
+        email
+    }).then(user => {
+        bcrypt.compare(password, user.password, function (err, resp) {
+            if (resp) {
+                jwt.sign({
+                    user
+                }, process.env.JWT_SECRET, {
+                    expiresIn: '3000s'
+                }, (err, token) => {
+                    res.status(200).json({
+                        token
+                    });
+                });
+            } else {
+                res.status(403).json({
+                    message: 'wrong password'
+                });
+            }
+        });
+    }).catch(() => res.status(403).json({
+        message: 'wrong user'
+    }));
+
+
+});
+
+app.get('/confirm/:email', (req, res) => {
+    var email = req.params.email;
+
+    users.update({
+        email: email
+    }, {
+        $set: {
+            confirmed: true
+        }
+    }).then(() => {
+        res.status(200).json('confirmed');
+    }).catch(err => res.status(403).json(err));
+});
+
+app.post('/upload', getUserEmailFromToken, (req, res) => {
+    let userTodos = db.get(req.token);
+    userTodos.insert({
+        test: req.body.test,
+        done: false
+    }).then(r => res.status(200).json(r));
+});
+
+app.get('/exercises', (req, res) => {
+    let exercises = db.get('exercises');
+    exercises.find({}).then(r =>{
+        console.log(r);
+        //TODO: map only titles and id's
+        //TODO: ? getUserEmailFromToken and get his scores ?
+        res.status(200).json(r);
+    });
+});
+
+app.get('/exercises/:id', getUserEmailFromToken, (req, res) => {
+    const id = req.params.id;
+    let exercises = db.get('exercises');
+    exercises.find({
+        _id:   id
+    }).then(r => res.status(200).json(r));
 });
 
 //app.listen(process.env.PORT || 5001, () => console.log('All is ok, sit back and relax!'));
