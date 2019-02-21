e753d02da05ae8091daffa24962c13e8e413e178
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-02-21 21:17:25 +0100

    get type E

--- routes/opposites.js
+++ routes/opposites.js
@@ -4,9 +4,19 @@ module.exports = function(app) {
     app.get('/getFlashCards', (req, res) => {
         console.log(process.env.MONGO_PW);
         try {
-            let labels = db.get('labels');
-            labels.find({}).then(r => {
-                return res.status(200).json(r);
+            let labels = db.get('exercises');
+            labels.find({ type: 'E' }).then(r => {
+                const exercises = r.map(x => x.exercise);
+                const randomExercise =
+                    exercises[Math.floor(Math.random() * exercises.length)];
+                return res
+                    .status(200)
+                    .json(
+                        randomExercise.map(x => ({
+                            '1': x.partA,
+                            '2': x.partB
+                        }))
+                    );
             });
         } catch (error) {
             res.status(500).json(error);
